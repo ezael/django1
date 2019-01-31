3fc3f40f6f3599036d145565159643f7ec4169e1
ezael <ewd.thierry@gmail.com>
2019-01-31 14:32:48 +0100

    go

--- aquawars/game/models.py
+++ aquawars/game/models.py
@@ -8,10 +8,25 @@ class Classe(models.Model):
     commerce = models.IntegerField()
     recherche = models.IntegerField()
     construction = models.IntegerField()
-    culture = models.IntegerField()
+    aquaculture = models.IntegerField()
     combat = models.IntegerField()
 
     def __str__(self):
         return self.nom
 
+
+class Serveur(models.Model):
+    nom = models.CharField(max_length=16)
+    lang = models.CharField(max_length=8)
+    res_prod = models.FloatField()
+    res_cap = models.FloatField()
+    att_light = models.FloatField()
+    att_medium = models.FloatField()
+    att_heavy = models.FloatField()
+    def_light = models.FloatField()
+    def_medium = models.FloatField()
+    def_heavy = models.FloatField()
+
+    def __str__(self):
+        return self.nom
 
